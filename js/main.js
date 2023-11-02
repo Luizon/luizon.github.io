a015c7f4f94a5863e3e907bdc53c9f3462ad02fa
Luis Fernando Cardenas Vega <luis.cardenas@sukarne.com>
2023-11-02 13:06:20 -0700

    Fixed some mobile responsive stuff, added a scroll down button for first screen
    I still have to add actual css to that button, I also have to add two more for the navbar, in order to let the user use them for navigate

--- js/main.js
+++ js/main.js
@@ -1,2 +1,18 @@
+const CONST_GREETINGS = 0;
+// const CONST_TECHNOLOGIES = 1;
+const CONST_PROJECTS = 1;
+const CONST_CONTACT = 3;
+
+// hides all mobile only stuff while user is not in a mobile device
+if (!/Mobi|Android|iPhone/i.test(navigator.userAgent)) {
+    let onlyMobileElements = document.getElementsByClassName("mobile-only");
+    for(element in onlyMobileElements) {
+      if(typeof onlyMobileElements[element] !== "object")
+        break;
+      onlyMobileElements[element].hidden = true;
+    }
+}
+
+
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
